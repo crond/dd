569683f8629d3aad201d75fd2dcc45c8715146ba
crondglobalname <crondglobalname@cms.com>
2012-11-03 09:12:19 +0530

    LFY USB-2

---
+++ LFY/usb2/.crond_usb.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/slacker/audacious/dd/LFY/usb2/crond_usb.ko := ld -r -m elf_i386 -T /usr/src/linux-2.6.37.6/scripts/module-common.lds --build-id  -o /home/slacker/audacious/dd/LFY/usb2/crond_usb.ko /home/slacker/audacious/dd/LFY/usb2/crond_usb.o /home/slacker/audacious/dd/LFY/usb2/crond_usb.mod.o
---
+++ LFY/usb2/.tmp_versions/crond_usb.mod
@@ -0,0 +1,2 @@
+/home/slacker/audacious/dd/LFY/usb2/crond_usb.ko
+/home/slacker/audacious/dd/LFY/usb2/crond_usb.o
